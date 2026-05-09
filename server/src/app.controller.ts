c488a8797c4ba3471da8b5de9a55e268c2479496
N-eeraj <neeraj7rajeena@gmail.com>
2026-05-09 17:13:51 +0530

    docs: add Scalar API documentation setup

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { All, Controller, Get, NotFoundException, Req } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NotFoundFilter } from './filters/not-found.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  // API documentation
+  const config = new DocumentBuilder()
+    .setTitle('Budgetify API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  app.use(
+    '/docs',
+    apiReference({
+      content: document,
+    }),
+  );
+
   app.useGlobalFilters(new NotFoundFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
