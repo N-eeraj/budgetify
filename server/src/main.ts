21439f579dec2629b35620b260a255aa212a49b9
N-eeraj <neeraj7rajeena@gmail.com>
2026-05-10 23:53:39 +0530

    feat: add class validator and global pipe to handle validations

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NotFoundFilter } from './filters/not-found.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { apiReference } from '@scalar/nestjs-api-reference';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,7 @@ async function bootstrap() {
     }),
   );
 
+  app.useGlobalPipes(new ValidationPipe())
   app.useGlobalFilters(new NotFoundFilter());
 
   await app.listen(process.env.PORT ?? 3000);
