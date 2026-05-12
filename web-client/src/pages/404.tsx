f0ddf9c74976cb649a003904b6d5df97c56262d2
anaghanisakar <anaghanisakar@gmail.com>
2026-05-12 15:32:10 +0530

    first commit

---
+++ web-client/src/pages/404.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function errorPage() {
+  return (
+    <div>404</div>
+  )
+}
+
+export default errorPage
---
+++ web-client/src/routers/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useRoutes } from 'react-router';
+import Homepage from '../pages/home'
+import Register from '../pages/register'
+import Login from '../pages/login'
+import Error from '../pages/404'
+
+
+function appRouter() {
+    const routes = useRoutes([
+        {
+            path: '/',
+            element: <Homepage />
+        },
+        {
+            path: '/register',
+            element: <Register />
+        },
+        {
+            path: '/login',
+            element: <Login />
+        },
+        {
+            path: '/404',
+            element: <Error />
+        },
+    ])
+
+    return routes
+}
+
+export default appRouter
