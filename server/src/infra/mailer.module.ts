d414bc4123eed8f8b24239f29b5f51d15d3a9185
N-eeraj <neeraj7rajeena@gmail.com>
2026-05-18 23:14:57 +0530

    refactor[server]: reorganize project folder structure

---
+++ server/src/infra/mailer.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/adapters/handlebars.adapter';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      template: {
+        dir: join(__dirname, 'assets', 'email-templates'),
+        adapter: new HandlebarsAdapter(),
+      },
+    }),
+  ],
+})
+export class InfraMailerModule {
+  
+}
